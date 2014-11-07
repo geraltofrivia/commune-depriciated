8b054ec639930b8843d3aff4bbb1c6550e8d05a8
Priyansh Trivedi <devl.priyansh@gmail.com>
2014-11-07 12:55:42 +0530

    v0.1.1 Templates Changes 	- Added template rendering 	- Made new templates folder and templates/index.html file 	- Messing around with CSS as of now

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res){ 
+ res.sendFile(__dirname+'/templates/index.html');
 });
 
 http.listen(3000, function(){
